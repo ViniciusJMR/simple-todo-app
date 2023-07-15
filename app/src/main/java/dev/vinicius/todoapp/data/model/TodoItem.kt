51e14e6b832cdb3bf950100104f9ae22a03aa0bf
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-07-15 09:01:50 -0300

    edit: added kapt hilt on build gradle

--- app/src/main/java/dev/vinicius/todoapp/data/model/TodoItem.kt
+++ app/src/main/java/dev/vinicius/todoapp/data/model/TodoItem.kt
@@ -2,14 +2,16 @@ package dev.vinicius.todoapp.domain
 
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 data class TodoItem(
     val name: String,
     val creationDate: LocalDate,
     val description: String
 ){
-    fun formatDate(): String {
-        val sdf = SimpleDateFormat.getDateInstance()
-        return sdf.format(creationDate)
+    fun getFormatedDate(): String {
+        val dtf = DateTimeFormatter.ofPattern("dd/MM")
+        return creationDate.format(dtf)
     }
 }
