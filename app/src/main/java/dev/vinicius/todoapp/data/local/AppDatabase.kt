52e4873b0df5c4294169b98b032e5d0e84997840
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-07-22 16:44:42 -0300

    add: sub todo item entity added to AppDatabase

--- app/src/main/java/dev/vinicius/todoapp/data/local/AppDatabase.kt
+++ app/src/main/java/dev/vinicius/todoapp/data/local/AppDatabase.kt
@@ -5,17 +5,20 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import androidx.room.migration.Migration
+import dev.vinicius.todoapp.data.local.dao.SubTodoDao
 import dev.vinicius.todoapp.data.local.dao.TodoItemDao
+import dev.vinicius.todoapp.data.model.SubTodoItem
 import dev.vinicius.todoapp.data.model.TodoItem
 import dev.vinicius.todoapp.util.Converters
 
 @Database(
-    entities = [TodoItem::class],
+    entities = [TodoItem::class, SubTodoItem::class],
     version = 2
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun todoItemDao(): TodoItemDao
+    abstract fun subTodoDao(): SubTodoDao
 
     companion object{
         val MIGRATION_1_2 = Migration(1,2) {
