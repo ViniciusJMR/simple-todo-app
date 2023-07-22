6e6115d02945ccd5101d454ceda84d25f4772537
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-07-22 19:20:46 -0300

    edit: added foreign relation to SubTodoItem

--- app/src/main/java/dev/vinicius/todoapp/data/model/SubTodoItem.kt
+++ app/src/main/java/dev/vinicius/todoapp/data/model/SubTodoItem.kt
@@ -1,10 +1,18 @@
 package dev.vinicius.todoapp.data.model
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = TodoItem::class,
+        parentColumns = ["id"],
+        childColumns = ["parentTodoId"],
+        onDelete = CASCADE
+    )]
+)
 data class SubTodoItem(
     @PrimaryKey
     val id: Long,
