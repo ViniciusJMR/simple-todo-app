1adba788ab6088be82697d8f8a541538e17fbeab
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-07-27 00:42:59 -0300

    edit: altered get todo detail file name

--- app/src/main/java/dev/vinicius/todoapp/domain/GetTodoDetailByIdUseCase.kt
+++ app/src/main/java/dev/vinicius/todoapp/domain/GetTodoDetailByIdUseCase.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class GetTodoDetailById @Inject constructor(
+class GetTodoDetailByIdUseCase @Inject constructor(
     private val todoItemRepository: TodoItemRepository
 ): UseCase<Long, TodoItemDTODetail> (){
     override suspend fun execute(param: Long): Flow<TodoItemDTODetail> = flow {
--- app/src/main/java/dev/vinicius/todoapp/viewmodel/DetailTodoViewModel.kt
+++ app/src/main/java/dev/vinicius/todoapp/viewmodel/DetailTodoViewModel.kt
@@ -6,11 +6,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.vinicius.todoapp.domain.GetTodoDetailById
+import dev.vinicius.todoapp.domain.GetTodoDetailByIdUseCase
 import dev.vinicius.todoapp.domain.dto.TodoItemDTODetail
 import dev.vinicius.todoapp.util.State
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,7 +17,7 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailTodoViewModel @Inject constructor(
     app: Application,
-    private val getTodoDetailById: GetTodoDetailById
+    private val getTodoDetailById: GetTodoDetailByIdUseCase
 ): AndroidViewModel(app){
 
     private val _state = MutableLiveData<State<TodoItemDTODetail>>()
