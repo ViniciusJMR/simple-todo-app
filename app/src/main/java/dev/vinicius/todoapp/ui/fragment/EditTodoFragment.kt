da4278a178391272ee5a3b72825340720f715e39
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-08-10 18:56:00 -0300

    add: edit todo fragment

---
+++ app/src/main/java/dev/vinicius/todoapp/ui/fragment/EditTodoFragment.kt
@@ -0,0 +1,89 @@
+package dev.vinicius.todoapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import dev.vinicius.todoapp.R
+import dev.vinicius.todoapp.databinding.FragmentEditTodoBinding
+import dev.vinicius.todoapp.domain.dto.TodoItemDTOInput
+import dev.vinicius.todoapp.util.State
+import dev.vinicius.todoapp.viewmodel.EditTodoViewModel
+import dev.vinicius.todoapp.viewmodel.SharedViewModel
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZoneOffset
+
+@AndroidEntryPoint
+class EditTodoFragment : Fragment() {
+
+    private lateinit var binding: FragmentEditTodoBinding
+    private val editTodoViewModel: EditTodoViewModel by viewModels()
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentEditTodoBinding.inflate(inflater, container, false)
+
+        setupUI()
+        setupObservers()
+
+        return binding.root
+    }
+
+    private fun setupObservers(){
+        editTodoViewModel.updateState.observe(viewLifecycleOwner){
+            when (it) {
+                is State.Loading -> {}
+                is State.Error -> {
+                    view?.let { view ->
+                        Snackbar
+                            .make(view, it.error.message ?: "Erro desconhecido", Snackbar.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                is State.Success -> {
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
+    private fun setupUI(){
+        binding.fragment = this
+        val todoOutput = sharedViewModel.getSelectedAsTodoItemOutput()
+        val todoInput = TodoItemDTOInput(
+            todoOutput.id,
+            todoOutput.name,
+            todoOutput.description,
+            todoOutput.endDate.toString(),
+        )
+        binding.todoItem = todoInput
+    }
+
+    fun saveTodo(v: View) {
+        editTodoViewModel.save(binding.todoItem!!)
+    }
+
+    fun setupDatePicker(v: View) {
+        val datePicker = MaterialDatePicker.Builder.datePicker().build()
+        datePicker.show(parentFragmentManager, "MATERIAL_DATE_PICKER")
+        datePicker.addOnPositiveButtonClickListener { millisecondsDate ->
+            val date = Instant.ofEpochMilli(millisecondsDate)
+                .atZone(ZoneId.of("America/Sao_Paulo"))
+                .withZoneSameInstant(ZoneId.ofOffset("UTC", ZoneOffset.UTC))
+                .toLocalDate()
+            binding.txtShowDateEdit.setText(date.toString())
+        }
+    }
+}
