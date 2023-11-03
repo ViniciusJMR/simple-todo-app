2b48a804bb5efc6b24eb9b21e016bc80f26fc98b
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-11-03 00:08:21 -0300

    add (TodoItemDTO): TodoItemDTOFinishTodo

--- app/src/main/java/dev/vinicius/todoapp/domain/dto/TodoItemDTO.kt
+++ app/src/main/java/dev/vinicius/todoapp/domain/dto/TodoItemDTO.kt
@@ -9,25 +9,33 @@ import java.time.Period
 import java.time.format.DateTimeFormatter
 import kotlin.math.absoluteValue
 
-data class TodoItemDTOOutput (
+data class TodoItemDTOOutput(
     val id: Long,
     val name: String,
     val creationDate: LocalDate,
     val endDate: LocalDate?,
-    val description: String
-){
+    val description: String,
+    var done: Boolean,
+) {
 
     private val dtf = DateTimeFormatter.ofPattern("dd/MM/yy")
 
     // Secondary constructor for mapping
     constructor(entity: TodoItem)
-            : this(entity.id, entity.name, entity.creationDate, entity.endDate, entity.description)
+            : this(
+        entity.id,
+        entity.name,
+        entity.creationDate,
+        entity.endDate,
+        entity.description,
+        entity.done
+    )
 
     fun getFormattedCreationDate(): String {
         return creationDate.format(dtf)
     }
 
-    fun getFormattedEndDate() : String? {
+    fun getFormattedEndDate(): String? {
         return endDate?.format(dtf)
     }
 
@@ -38,7 +46,7 @@ data class TodoItemDTOOutput (
                 .days
         }
 
-    fun getDaysLeftResource() : String{
+    fun getDaysLeftResource(): String {
         val context = App.getContext()
         val period = endDate?.let {
             Period
@@ -56,24 +64,31 @@ data class TodoItemDTOOutput (
                 context.resources.getQuantityString(
                     R.plurals.number_of_days_negative,
                     days.absoluteValue,
-                    days.absoluteValue)
+                    days.absoluteValue
+                )
         }
         return stringResource
     }
 
-    fun toEntity() = TodoItem(id, name, creationDate, endDate, description)
+    fun toEntity() = TodoItem(id, name, creationDate, endDate, description, done)
 }
 
-data class TodoItemDTOInput (
+data class TodoItemDTOInput(
     var id: Long = 0,
     var name: String = "",
     var description: String = "",
     var endDate: String = "",
+    var done: Boolean = false,
     var subTodoList: List<SubTodoItemShow> = listOf()
 )
 
-data class TodoItemDTODetail (
-    var todoItemOutput:TodoItemDTOOutput? = null,
+data class TodoItemDTOFinishTodo(
+    val todoDetail: TodoItemDTODetail,
+    val forceSave: Boolean,
+)
+
+data class TodoItemDTODetail(
+    var todoItemOutput: TodoItemDTOOutput? = null,
     var subTodoList: MutableList<SubTodoItemShow>? = null
-){
+) {
 }
