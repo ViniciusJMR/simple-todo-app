c0223ec86c845aae243289e45db8e8dc7e2a3170
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-11-06 22:48:32 -0300

    add: GetAllFinishedTodo and NotFinished

---
+++ app/src/main/java/dev/vinicius/todoapp/domain/todousecase/GetAllFinishedTodoUseCase.kt
@@ -0,0 +1,41 @@
+package dev.vinicius.todoapp.domain.todousecase
+
+import dev.vinicius.todoapp.data.local.repository.impl.TodoItemRepository
+import dev.vinicius.todoapp.data.model.TodoWithSubTodos
+import dev.vinicius.todoapp.domain.dto.SubTodoItemShow
+import dev.vinicius.todoapp.domain.dto.TodoItemDTODetail
+import dev.vinicius.todoapp.domain.dto.TodoItemDTOOutput
+import dev.vinicius.todoapp.util.UseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetAllFinishedTodoUseCase @Inject constructor(
+    private val todoItemRepository: TodoItemRepository
+) : UseCase.NoParam<MutableList<TodoItemDTODetail>>() {
+
+    override suspend fun execute(): Flow<MutableList<TodoItemDTODetail>> = flow {
+        todoItemRepository.getAllWithSubTodos()
+            .collect { list ->
+                val newList =
+                    list
+                        .filter { it.todoItem.done == true}
+                        .map { todoWithSubTodos ->
+                            TodoItemDTODetail(
+                                TodoItemDTOOutput(todoWithSubTodos.todoItem),
+                                mapSubTodosToDTO(todoWithSubTodos)
+                            )
+                        }
+                        .toMutableList()
+
+                emit(newList)
+            }
+    }
+
+    private fun mapSubTodosToDTO(todoWithSubTodos: TodoWithSubTodos) =
+        todoWithSubTodos.subTodolist
+            .map { subTodoItem ->
+                SubTodoItemShow(subTodoItem)
+            }
+            .toMutableList()
+}
